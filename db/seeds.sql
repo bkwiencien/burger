40d3d755d76db51a439dd68eb4f455f86b8ae998
Bob Kwiencien <bkwiencien@gmail.com>
2017-10-18 14:49:32 -0400

    database stuff

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 insert into burgers (burger_name) values ('classic cheeseburger');
 insert into burgers (burger_name) values ('double cheeseburger');
-insert into burgers (burger_name) values ('triple cheeseburger')
+insert into burgers (burger_name) values ('triple cheeseburger');
